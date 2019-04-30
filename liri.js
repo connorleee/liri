2d84a5cf5780e5a5a5267b934029f34e70e01a7c
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-04-29 21:08:53 -0700

    finished concert-this call. added error handling to concert-this call and while loop to loop through only the existing upcoming events.

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const keys = require("./keys.js");
 const axios = require("axios");
 const Spotify = require('node-spotify-api');
+const moment = require("moment")
 
 const spotify = new Spotify(keys.spotify);
 
@@ -18,9 +19,51 @@ const request = process.argv.slice(3).join(" ")
 
 
 switch (action) {
+    case "concert-this":
+        var artist = request;
+        var url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
+
+        axios.get(url).then(
+            function (response) {
+                console.log(`Artist: ${artist}`);
+
+                // Error handler
+                if (typeof response.data[0] === "undefined") {
+                    return console.log("Artist has no upcoming events or is not in database");
+                }
+
+                // loop through the first 5 venues 
+                let i = 0
+                while (typeof response.data[i] != "undefined") {
+                    // only display first 5 results
+                    if(i>4){
+                        break
+                    }
+
+                    console.log(`\n-------------\n Event ${i + 1} \n-------------`)
+
+                    // Venue
+                    console.log(`Venue: ${response.data[i].venue.name}`)
+
+                    // Venue Location
+                    console.log(`Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}`)
+
+                    // Date of event using moment "MM/DD/YYYY"
+                    console.log(`Date: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}`);
+
+                    i++
+                };
+            }
+        )
+        break;
+
+    case "spotify-this-song":
+
+        break;
+
     case "movie-this":
-        let t = request
-        let url = `http://www.omdbapi.com/?apikey=${omdb.key}&t=${t}`
+        var t = request;
+        var url = `http://www.omdbapi.com/?apikey=${omdb.key}&t=${t}`;
 
         axios.get(url).then(
             function (response) {
@@ -32,7 +75,7 @@ switch (action) {
 
                 // IMDB Rating of the movie.
                 console.log(`IMDb rating is: ${response.data.imdbRating}`);
-                
+
                 // Rotten Tomatoes Rating of the movie.
                 console.log(`Rotten Tomatoes rating is: ${response.data.Ratings[1].Value}`);
 
@@ -51,15 +94,13 @@ switch (action) {
         );
         break;
 
-
-
     case "do-what-it-says":
         fs.readFile("random.txt", "utf8", function (error, data) {
             if (error) {
                 return console.log(error);
             };
 
-            console.log(data)
+            console.log(data);
         });
 
 }
